88873db0df262649faf817bdb68c3f7d3b54f501
tilatti <tilatti@CenturionCeleron.(none)>
2010-05-08 01:07:36 +0200

    Gopher client can get files in subdirectory

--- src/parse_request.hs
+++ src/parse_request.hs
@@ -4,23 +4,29 @@ import System.Directory
 import System.IO
 import Monad (join)
 
+
 reply_request :: FilePath -> Handle -> IO ()
+reply_request "" channel =
+  do
+    get_cache_content "./" channel
 reply_request request_line channel =
   do
     is_dir <- doesDirectoryExist request_line
     is_file <- doesFileExist request_line
-    get_ressource request_line is_dir channel
+    get_ressource request_line (is_dir, is_file) channel
 
-get_ressource :: FilePath -> Bool -> Handle -> IO ()
-get_ressource request_line isDir channel
-  | isDir = get_cache_content request_line channel
- -- | otherwise = get_file_content request_line channel
-  | isFile = get_file_content request_line channel
-  | oterwise = ()
+
+get_ressource :: FilePath -> (Bool, Bool) -> Handle -> IO ()
+get_ressource request_line (is_dir, is_file) channel
+  | is_dir = get_cache_content request_line channel
+  | is_file = get_file_content request_line channel
+  | otherwise = do print "Unknow descriptor"
+
 
 get_cache_content :: FilePath -> Handle -> IO ()
 get_cache_content dir_path channel =
-  get_file_content (dir_path ++ ".cache") channel
+  get_file_content (dir_path ++ "/.cache") channel
+
 
 get_file_content :: FilePath -> Handle  -> IO ()
 get_file_content file_path channel =
