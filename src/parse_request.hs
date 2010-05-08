4feca4775b86759128ee60eb92507fe43817b533
tilatti <tilatti@CenturionCeleron.(none)>
2010-05-08 11:50:58 +0200

    Add remote function invocation basic support

--- src/parse_request.hs
+++ src/parse_request.hs
@@ -3,6 +3,8 @@ module ReplyRequest where
 import System.Directory
 import System.IO
 import Monad (join)
+import FunctionMap
+import Maybe
 
 
 reply_request :: FilePath -> Handle -> IO ()
@@ -20,12 +22,15 @@ get_ressource :: FilePath -> (Bool, Bool) -> Handle -> IO ()
 get_ressource request_line (is_dir, is_file) channel
   | is_dir = get_cache_content request_line channel
   | is_file = get_file_content request_line channel
-  | otherwise = do print "Unknow descriptor"
+  | otherwise = apply_reply_function request_line channel
+-- | otherwise = do print "Unknow descriptor"
 
 
 get_cache_content :: FilePath -> Handle -> IO ()
 get_cache_content dir_path channel =
-  get_file_content (dir_path ++ "/.cache") channel
+  do
+    get_file_content (dir_path ++ "/.cache") channel
+    get_func_map_content channel
 
 
 get_file_content :: FilePath -> Handle  -> IO ()
@@ -33,3 +38,22 @@ get_file_content file_path channel =
   do
     s <- readFile file_path
     hPutStr channel s
+
+
+
+apply_reply_function :: String -> Handle -> IO ()
+apply_reply_function request_line channel =
+ let
+   reply_function = fromJust (getFunction request_line initMap)
+ in
+  do
+    hPutStr channel (reply_function request_line)
+
+get_func_map_content :: Handle -> IO ()
+get_func_map_content channel = get_func_map_content_rec channel (extractFunMap initMap)
+  where
+   get_func_map_content_rec ::  Handle -> [(String, ReplyFunction)] -> IO ()
+   get_func_map_content_rec channel [] = do hPutStrLn channel ""
+   get_func_map_content_rec channel ((key, func):map) =
+     do
+       hPutStrLn channel key
