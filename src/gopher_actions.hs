687ef8769327b9f54dd37f1e2c4c1077758a5242
tilatti <tilatti@CenturionCeleron.(none)>
2010-10-15 22:17:48 +0200

    Add file

---
+++ src/gopher_actions.hs
@@ -0,0 +1,65 @@
+module GopherActions where
+
+type GopherReply = Connection -> String -> IO Bool
+
+actionIsDir :: GopherReply
+actionIsDir conn request_line =
+  do
+    is_dir <- doesDirectoryExist request_line
+    if (is_dir)
+      then
+        get_dir_content request_line (channel conn) >>
+	return True
+      else
+	return False
+
+actionIsFile :: GopherReply
+actionIsFile conn request_line =
+  do
+    is_file <- doesFileExist request_line
+    if (is_file)
+      then
+	get_file_content request_line (channel conn) >>
+	return True
+      else
+	return False
+
+actionIsCallToExec :: GopherReply
+actionIsCallToExec conn request_line =
+  let
+    exec_info = parseCallRequest request_line
+  in
+    do
+      if (isJust exec_info)
+        then
+	  do
+	    callExecutable (fst (fromJust exec_info)) (channel conn)
+	    return True
+	else
+	  return False
+
+actionIsCallToFun :: GopherReply
+actionIsCallToFun conn request =
+  do
+    apply_reply_function conn request
+    return True
+
+gopher_replies :: [GopherReply]
+gopher_replies = [actionIsDir,
+		  actionIsFile,
+		  actionIsCallToExec,
+		  actionIsCallToFun]
+
+
+applyGopherReply :: Connection -> String -> [GopherReply] -> IO ()
+applyGopherReply connection request [] = return ()
+applyGopherReply connection request (action : xs) =
+  do
+    action_result <- action connection request
+    if (action_result)
+      then
+      	return ()
+      else
+	applyGopherReply connection request xs
+
+
